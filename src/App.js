1f0f235879f222f00dff63d6986a573af3cf3544
gilbertovelazquez <gnilberto_1@hotmail.com>
2022-09-01 21:36:29 -0500

    desafio stock contador

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import NavBar from './componentes/navbar/NavBar'
+import Inicio from './componentes/paginas/Inicio'
+import Prueba from './componentes/paginas/Prueba'
+import Test from './componentes/paginas/Test'
+import ItemListContainer from './componentes/navbar/ItemListContainer'
+import { useEffect, useState } from 'react';
+
+
+
+function App() {
+
+  const [counter, setCounter] = useState (5)
+
+  useEffect (() => {
+    console.log('efecto...');
+  } , [counter])
+
+  const handleClick = () => {
+    console.log('hiciste click');
+    setCounter(counter + 1) 
+  }
+
+
+  return (
+    <div className="App">
+      <Router>
+          <NavBar/>
+
+          <div className='my-2'>
+          <strong>Contador: {counter}</strong>
+          </div>
+
+          <button onClick={handleClick} className='btn btn-primary my-4'>Click</button>
+
+          <ItemListContainer/>
+          <Routes>
+            <Route exact path="/" element={<Inicio/>}/>
+            <Route  path="/Prueba" element={<Prueba/>}/>
+            <Route  path='/Test' element={<Test/>}/>
+          </Routes>
+       </Router>
+    </div>
+  );
+}
+
+export default App;
